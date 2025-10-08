a54609f9b7242018ac30216bb950662939e97987
Kristjan Pullerits <kristjan.pullerits@olink.com>
2025-10-08 13:14:04 +0200

    Edit filter to 5 and add geom_smooth()

--- script.R
+++ script.R
@@ -3,9 +3,10 @@ library(ggplot2)
 
 # filter
 filtered_mtcars <- mtcars %>% 
-  filter(cyl > 2) 
+  filter(cyl > 5) 
 
 # plot
 filtered_mtcars %>% 
   ggplot(aes(x = cyl, y = hp))+
-  geom_point()
+  geom_point()+
+  geom_smooth()
